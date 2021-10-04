b5e213ccffdd972c51659cf20780ff92a6c9c105
william villani stiehler <william.stiehler@olist.com>
2021-10-04 11:05:24 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/wstiehler/OrientacaoObjeto/contas"
+)
+
+func main() {
+	contaDoDario := contas.ContaCorrente{Titular: "Dario", Saldo: 400}
+	contaDaJuce := contas.ContaCorrente{Titular: "Juce", Saldo: 100}
+
+	status := contaDoDario.Transferir(200, &contaDaJuce)
+
+	fmt.Println(status)
+	fmt.Println(contaDoDario)
+	fmt.Println(contaDaJuce)
+
+}
